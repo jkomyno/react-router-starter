51baae9dbdd570740857573f1b11265d834af6c9
jkomyno <12381818+jkomyno@users.noreply.github.com>
2025-07-03 16:23:15 +0400

    feat: implement "resolve" workaround in "vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -11,4 +12,19 @@ export default defineConfig({
     reactRouter(),
     tsconfigPaths(),
   ],
+
+  /**
+   * Note: this is a workaround that allows Vite to import `@prisma/client/runtime/*` from the
+   * correct location (node_modules), rather than from the project directory.
+   * 
+   * See line 79 in `./app/generated/prisma/internal/class.ts` for the import statement.
+   */
+  resolve: {
+    alias: [
+      {
+        find: /@prisma\/client\/runtime/,
+        replacement: path.resolve(__dirname, "node_modules", "@prisma", "client", "runtime"),
+      },
+    ],
+  },
 });
